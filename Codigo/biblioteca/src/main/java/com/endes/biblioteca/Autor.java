21e00c43cf66961d92bc81179deaa1ed47ad3a2d
David Montes Diaz <dmondia679@g.educaand.es>
2024-04-16 19:45:04 +0200

    nueva version en umbrello

--- Codigo/biblioteca/src/main/java/com/endes/biblioteca/Autor.java
+++ Codigo/biblioteca/src/main/java/com/endes/biblioteca/Autor.java
@@ -3,6 +3,28 @@ package com.endes.biblioteca;
 public class Autor {
     private String name;
     private String biography;
+    
+    
+    
+	public Autor(String name, String biography) {
+		super();
+		this.name = name;
+		this.biography = biography;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBiography() {
+		return biography;
+	}
+	public void setBiography(String biography) {
+		this.biography = biography;
+	}
 
+    
+    
     // Constructor, getters y setters aquí
 }
